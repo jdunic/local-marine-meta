05f8fb9fa78b4c91ad52f225548156e72f2c9050
Jillian Dunic <jdunic@gmail.com>
2016-01-31 16:14:23 -0800

    correctly identify the unique fl data

--- Create_master_fl_combined_df.R
+++ Create_master_fl_combined_df.R
@@ -343,27 +343,45 @@ write.csv(temp_data_combined, "Data_outputs/spatial_data_with_temp_data.csv", ro
 # ------------------------------------------------------------------------------
 # Combine the extracted spatial data 
 # ------------------------------------------------------------------------------
-
 imps <- readr::read_csv('Data_outputs/spatial_data_with_cumulative_impacts.csv')
 invs <- readr::read_csv('Data_outputs/spatial_data_with_invasives.csv')
 nuts <- readr::read_csv('Data_outputs/spatial_data_with_nutrients.csv')
 pest <- readr::read_csv('Data_outputs/spatial_data_with_pesticides.csv')
 temp <- readr::read_csv('Data_outputs/spatial_data_with_temp_data.csv')
 
+imps <- dplyr::select(imps, row, mean_imps)
+invs <- dplyr::select(invs, row, mean_invs)
+nuts <- dplyr::select(nuts, row, mean_nuts)
+pest <- dplyr::select(pest, row, mean_pests)
+temp <- dplyr::select(temp, row, mean_lin_change, mean_vocc)
 
-imps <- select(imps, row, mean_imps)
-invs <- select(invs, row, mean_invs)
-nuts <- select(nuts, row, mean_nuts)
-pest <- select(pest, row, mean_pests)
-temp <- select(temp, row, mean_lin_change, mean_vocc)
+spatial_data <- read.csv('master_data/SiteSpatialData.csv', stringsAsFactors = FALSE)
+spatial_data <- 
+  spatial_data %>% 
+  mutate(Reference = trimws(.$Reference), 
+         Site = trimws(.$Site), 
+         Shape = trimws(.$Shape)) %>% 
+  mutate(site_id = paste(.$Study.ID, .$Site, sep = "_"))
 
+# Add rows so that I can create the spatial lines dataframe
+spatial_data$row <- seq_along(spatial_data$Study.ID)
 
-fl_combined <- 
+all_spatial_data <- 
 left_join(spatial_data, imps, by = c('row' = 'row')) %>% 
   left_join(., invs, by = c('row' = 'row')) %>% 
   left_join(., nuts, by = c('row' = 'row')) %>% 
   left_join(., pest, by = c('row' = 'row')) %>% 
   left_join(., temp, by = c('row' = 'row')) %>% 
   as_data_frame(.)
+
+fl_combined <- 
+  full_join(all_spatial_data, fl, by = c('site_id' = 'site_id')) %>%
+  group_by(id) %>% 
+  summarise(mean_imps = mean(mean_imps, na.rm = TRUE), 
+            mean_invs = mean(mean_invs, na.rm = TRUE), 
+            mean_nuts = mean(mean_nuts, na.rm = TRUE), 
+            mean_pests = mean(mean_pests, na.rm = TRUE), 
+            mean_lin_change = mean(mean_lin_change, na.rm = TRUE), 
+            mean_vocc = mean(mean_vocc, na.rm = TRUE))
 
 write.csv(fl_combined, 'Data_outputs/fl_combined.csv', row.names = FALSE)
