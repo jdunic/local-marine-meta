ee1772b88065caa7da925f14dca0667ed01fcf07
J Dunic <jdunic@gmail.com>
2018-08-01 17:52:03 -0700

    fix plotting of temp facet intervals

--- R/05-combined-driver-effect-plot.R
+++ R/05-combined-driver-effect-plot.R
@@ -1,6 +1,6 @@
 library(disco)
 
-temp = c(-1, -0.5, 0, 0.5, 1)
+temp = c(-1, -0.5, -0.001, 0.001, 0.5, 1)
 invs = seq(from = 0, to = 160000, by = 100)
 duration = as.vector(c(5, 20), mode = 'integer')
 nuts = seq(from = 0, to = 200, by = 1)
@@ -19,14 +19,18 @@ g_preds <-
                                  nuts == nut_quantiles$`100%` ~ 'max (185)')) %>% 
   mutate(change = case_when(ci.ub < 0 ~ 'loss', ci.lb > 0 ~ 'gain', ci.lb < 0 & ci.ub > 0 ~ 'no change')) %>% 
   mutate(change = factor(change, level = c('gain', 'no change', 'loss'))) %>%
-
-test <- 
-  g_preds %>%   
   mutate(temp = case_when(temp <= -1 ~ '-1 <= temp', 
-                          temp > -1 & temp <= 0.5 ~ '-1 < temp <= -0.5', 
-                          temp > -0.5 & temp <= 0 ~ '- 0.5 < temp <= 0',
-                          temp >= 0 & temp <= 0.5 ~ '0 < temp <= 0.5', 
-                          temp >= 1 ~ '1 <= temp'))
+                          temp >  -1 & temp <= -0.5 ~ '-1 < temp <= -0.5', 
+                          temp >  -0.5 & temp < 0 ~ '-0.5 < temp <= 0',
+                          temp >  0 & temp < 0.5 ~ '0 <= temp < 0.5', 
+                          temp >=  0.5 & temp < 1 ~ '0.5 < temp <= 1', 
+                          temp >=  1 ~ '1 <= temp')) %>% 
+  mutate(temp = factor(temp, levels = c('-1 <= temp', 
+                                         '-1 < temp <= -0.5', 
+                                         '-0.5 < temp <= 0', 
+                                         '0 <= temp < 0.5',
+                                         '0.5 < temp <= 1', 
+                                         '1 <= temp')))
 
 ggplot() + 
   theme(legend.background = element_blank(), 
@@ -37,9 +41,8 @@ ggplot() +
         panel.grid.minor = element_blank(), 
         strip.background = element_blank(), 
         plot.background = element_blank()) + 
-  geom_point(data = test, aes(x = invs, y = nuts, colour = change), shape = 15) + 
-  #scale_colour_manual(values = c("#a1d76a", "grey90", "#e9a3c9")) + 
-  facet_grid(factor(duration) ~ factor(temp)) + 
+  geom_point(data = g_preds, aes(x = invs, y = nuts, colour = change), shape = 15) + 
+  facet_grid(factor(duration) ~ temp) + 
   #scale_x_log10() + 
   #scale_y_log10(labels=scales::trans_format('log10', scales::math_format(10^.x))) + 
   geom_point(
@@ -47,10 +50,17 @@ ggplot() +
            filter(!is.na(yi_SppR_ROM) & !is.na(vi_SppR_ROM) & !is.na(mean_invs) & 
                   !is.na(mean_nuts) & !is.na(sliced_ltc)) %>% 
            mutate(temp = case_when(sliced_ltc <= -1 ~ '-1 <= temp', 
-                                   sliced_ltc > -1 & sliced_ltc <= 0.5 ~ '-1 < temp <= -0.5', 
-                                   sliced_ltc > -0.5 & sliced_ltc <= 0 ~ '- 0.5 < temp <= 0',
-                                   sliced_ltc >= 0 & sliced_ltc <= 0.5 ~ '0 < temp <= 0.5', 
+                                   sliced_ltc > -1 & sliced_ltc <= -0.5 ~ '-1 < temp <= -0.5', 
+                                   sliced_ltc > -0.5 & sliced_ltc <= -0.001 ~ '-0.5 < temp <= 0',
+                                   sliced_ltc >= 0.001 & sliced_ltc <= 0.5 ~ '0 <= temp < 0.5', 
+                                   sliced_ltc >  0.5 & sliced_ltc <= 1 ~ '0.5 < temp <= 1',
                                    sliced_ltc >= 1 ~ '1 <= temp')) %>% 
+           mutate(temp = factor(temp, levels = c('-1 <= temp', 
+                                         '-1 < temp <= -0.5', 
+                                         '-0.5 < temp <= 0', 
+                                         '0 <= temp < 0.5',
+                                         '0.5 < temp <= 1', 
+                                         '1 <= temp'))) %>% 
            mutate(duration = case_when(Duration <= 10 ~ 5, Duration > 10 ~ 20)) %>% 
            mutate(change = case_when(yi_SppR_ROM + vi_SppR_ROM < 0 ~ 'loss', yi_SppR_ROM - vi_SppR_ROM > 0 ~ 'gain', yi_SppR_ROM - vi_SppR_ROM < 0 & yi_SppR_ROM + vi_SppR_ROM > 0 ~ 'no change')) %>% 
           mutate(change = factor(change, level = c('gain', 'no change', 'loss')))
@@ -58,10 +68,17 @@ ggplot() +
     aes(x = mean_invs + 0.1, y = mean_nuts + 0.001, colour = change)) + 
   geom_point(data = no_event %>% filter(!is.na(yi_SppR_ROM) & !is.na(vi_SppR_ROM) & !is.na(mean_invs) & 
          !is.na(mean_nuts) & !is.na(sliced_ltc)) %>% mutate(temp = case_when(sliced_ltc <= -1 ~ '-1 <= temp', 
-                                   sliced_ltc > -1 & sliced_ltc <= 0.5 ~ '-1 < temp <= -0.5', 
-                                   sliced_ltc > -0.5 & sliced_ltc <= 0 ~ '- 0.5 < temp <= 0',
-                                   sliced_ltc >= 0 & sliced_ltc <= 0.5 ~ '0 < temp <= 0.5', 
+                                   sliced_ltc > -1 & sliced_ltc <= -0.5 ~ '-1 < temp <= -0.5', 
+                                   sliced_ltc > -0.5 & sliced_ltc <= -0.001 ~ '-0.5 < temp <= 0',
+                                   sliced_ltc >= 0.001 & sliced_ltc <= 0.5 ~ '0 <= temp < 0.5', 
+                                   sliced_ltc >  0.5 & sliced_ltc <= 1 ~ '0.5 < temp <= 1',
                                    sliced_ltc >= 1 ~ '1 <= temp')) %>% 
+  mutate(temp = factor(temp, levels = c('-1 <= temp', 
+                                         '-1 < temp <= -0.5', 
+                                         '-0.5 < temp <= 0', 
+                                         '0 <= temp < 0.5',
+                                         '0.5 < temp <= 1', 
+                                         '1 <= temp'))) %>% 
   mutate(duration = case_when(Duration <= 10 ~ 5, Duration > 10 ~ 20)), 
     aes(x = mean_invs + 0.1, y = mean_nuts + 0.001), shape = 21, colour = 'black') + 
   scale_color_disco(palette = "sunset")
