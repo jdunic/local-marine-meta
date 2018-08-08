74f7cbd0437d5003884e7966083aafe82f601c7a
J Dunic <jdunic@gmail.com>
2018-08-08 09:43:29 -0700

    removed points because of variability in Duration

--- R/05-combined-driver-effect-plot.R
+++ R/05-combined-driver-effect-plot.R
@@ -1,7 +1,5 @@
-library(disco)
-
-temp = c(-1, -0.5, -0.001, 0.001, 0.5, 1)
-invs = seq(from = 0, to = 160000, by = 100)
+temp = c(-0.5, -0.001, 0.001, 0.5)
+invs = seq(from = 0, to = 160000, by = 1000)
 duration = as.vector(c(5, 20), mode = 'integer')
 nuts = seq(from = 0, to = 200, by = 1)
 
@@ -19,19 +17,18 @@ g_preds <-
                                  nuts == nut_quantiles$`100%` ~ 'max (185)')) %>% 
   mutate(change = case_when(ci.ub < 0 ~ 'loss', ci.lb > 0 ~ 'gain', ci.lb < 0 & ci.ub > 0 ~ 'no change')) %>% 
   mutate(change = factor(change, level = c('gain', 'no change', 'loss'))) %>%
-  mutate(temp = case_when(temp <= -1 ~ '-1 <= temp', 
-                          temp >  -1 & temp <= -0.5 ~ '-1 < temp <= -0.5', 
-                          temp >  -0.5 & temp < 0 ~ '-0.5 < temp <= 0',
-                          temp >  0 & temp < 0.5 ~ '0 <= temp < 0.5', 
-                          temp >=  0.5 & temp < 1 ~ '0.5 < temp <= 1', 
-                          temp >=  1 ~ '1 <= temp')) %>% 
-  mutate(temp = factor(temp, levels = c('-1 <= temp', 
-                                         '-1 < temp <= -0.5', 
-                                         '-0.5 < temp <= 0', 
-                                         '0 <= temp < 0.5',
-                                         '0.5 < temp <= 1', 
-                                         '1 <= temp')))
+  mutate(temp = case_when(temp <=  -0.5 ~ '< -0.5', 
+                          temp >   -0.5 & temp < 0 ~ '-0.5 to 0',
+                          temp >    0 & temp < 0.5 ~ '0 to -0.5', 
+                          temp >=   0.5 ~ '> 0.5')) %>% 
+  mutate(temp = factor(temp, levels = c('< -0.5', 
+                                        '-0.5 to 0', 
+                                        '0 to -0.5',
+                                        '> 0.5'))) %>% 
+  mutate(duration = case_when(duration == 5 ~ '5 years', duration == 20 ~ '20 years')) %>% 
+  mutate(duration = factor(duration, levels = c('5 years', '20 years')))
 
+dev.new(width = 10, height = 5)
 ggplot() + 
   theme(legend.background = element_blank(), 
         legend.key = element_blank(), 
@@ -40,48 +37,54 @@ ggplot() +
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
         strip.background = element_blank(), 
-        plot.background = element_blank()) + 
-  geom_point(data = g_preds, aes(x = invs, y = nuts, colour = change), shape = 15) + 
-  facet_grid(factor(duration) ~ temp) + 
+        plot.background = element_blank(),
+        strip.text.y = element_text(angle = 0), 
+        plot.title = element_text(hjust = 0.5, size = 10)) + 
+  geom_point(data = filter(g_preds), aes(x = invs, y = nuts, colour = change), shape = 15) + 
+  scale_colour_manual(values = c('#0571b0', 'grey90', '#ca0020')) + 
   #scale_x_log10() + 
   #scale_y_log10(labels=scales::trans_format('log10', scales::math_format(10^.x))) + 
-  geom_point(
-    data = no_event %>% 
-           filter(!is.na(yi_SppR_ROM) & !is.na(vi_SppR_ROM) & !is.na(mean_invs) & 
-                  !is.na(mean_nuts) & !is.na(sliced_ltc)) %>% 
-           mutate(temp = case_when(sliced_ltc <= -1 ~ '-1 <= temp', 
-                                   sliced_ltc > -1 & sliced_ltc <= -0.5 ~ '-1 < temp <= -0.5', 
-                                   sliced_ltc > -0.5 & sliced_ltc <= -0.001 ~ '-0.5 < temp <= 0',
-                                   sliced_ltc >= 0.001 & sliced_ltc <= 0.5 ~ '0 <= temp < 0.5', 
-                                   sliced_ltc >  0.5 & sliced_ltc <= 1 ~ '0.5 < temp <= 1',
-                                   sliced_ltc >= 1 ~ '1 <= temp')) %>% 
-           mutate(temp = factor(temp, levels = c('-1 <= temp', 
-                                         '-1 < temp <= -0.5', 
-                                         '-0.5 < temp <= 0', 
-                                         '0 <= temp < 0.5',
-                                         '0.5 < temp <= 1', 
-                                         '1 <= temp'))) %>% 
-           mutate(duration = case_when(Duration <= 10 ~ 5, Duration > 10 ~ 20)) %>% 
-           mutate(change = case_when(yi_SppR_ROM + vi_SppR_ROM < 0 ~ 'loss', yi_SppR_ROM - vi_SppR_ROM > 0 ~ 'gain', yi_SppR_ROM - vi_SppR_ROM < 0 & yi_SppR_ROM + vi_SppR_ROM > 0 ~ 'no change')) %>% 
-          mutate(change = factor(change, level = c('gain', 'no change', 'loss')))
-  , 
-    aes(x = mean_invs + 0.1, y = mean_nuts + 0.001, colour = change)) + 
-  geom_point(data = no_event %>% filter(!is.na(yi_SppR_ROM) & !is.na(vi_SppR_ROM) & !is.na(mean_invs) & 
-         !is.na(mean_nuts) & !is.na(sliced_ltc)) %>% mutate(temp = case_when(sliced_ltc <= -1 ~ '-1 <= temp', 
-                                   sliced_ltc > -1 & sliced_ltc <= -0.5 ~ '-1 < temp <= -0.5', 
-                                   sliced_ltc > -0.5 & sliced_ltc <= -0.001 ~ '-0.5 < temp <= 0',
-                                   sliced_ltc >= 0.001 & sliced_ltc <= 0.5 ~ '0 <= temp < 0.5', 
-                                   sliced_ltc >  0.5 & sliced_ltc <= 1 ~ '0.5 < temp <= 1',
-                                   sliced_ltc >= 1 ~ '1 <= temp')) %>% 
-  mutate(temp = factor(temp, levels = c('-1 <= temp', 
-                                         '-1 < temp <= -0.5', 
-                                         '-0.5 < temp <= 0', 
-                                         '0 <= temp < 0.5',
-                                         '0.5 < temp <= 1', 
-                                         '1 <= temp'))) %>% 
-  mutate(duration = case_when(Duration <= 10 ~ 5, Duration > 10 ~ 20)), 
-    aes(x = mean_invs + 0.1, y = mean_nuts + 0.001), shape = 21, colour = 'black') + 
-  scale_color_disco(palette = "sunset")
+  #geom_point(
+  #  data = no_event %>% 
+  #         filter(!is.na(yi_SppR_ROM) & !is.na(vi_SppR_ROM) & !is.na(mean_invs) & 
+  #                !is.na(mean_nuts) & !is.na(sliced_ltc)) %>% 
+  #         mutate(temp = case_when(sliced_ltc <= -0.5 ~ '< -0.5', 
+  #                                 sliced_ltc > -0.5 & sliced_ltc <= -0.001 ~ '-0.5 to 0',
+  #                                 sliced_ltc >= 0.001 & sliced_ltc < 0.5 ~ '0 to -0.5', 
+  #                                 sliced_ltc >= 0.5 ~ '> 0.5')) %>% 
+  #         mutate(temp = factor(temp, levels = c('< -0.5', 
+  #                                               '-0.5 to 0', 
+  #                                               '0 to -0.5', 
+  #                                               '> 0.5'))) %>% 
+  #         mutate(duration = case_when(Duration <= 10 ~ '5 years', Duration > 10 ~ '20 years')) %>%
+  #         mutate(duration = factor(duration, levels = c('5 years', '20 years'))) %>%  
+  #         mutate(change = case_when(yi_SppR_ROM + vi_SppR_ROM < 0 ~ 'loss', yi_SppR_ROM - vi_SppR_ROM > 0 ~ 'gain', yi_SppR_ROM - vi_SppR_ROM < 0 & yi_SppR_ROM + vi_SppR_ROM > 0 ~ 'no change')), 
+  #  aes(x = mean_invs + 0.1, y = mean_nuts + 0.001, colour = change)) + 
+  #geom_point(
+  #  data = no_event %>% 
+  #         filter(!is.na(yi_SppR_ROM) & !is.na(vi_SppR_ROM) & !is.na(mean_invs) & 
+  #                !is.na(mean_nuts) & !is.na(sliced_ltc)) %>% 
+  #         mutate(temp = case_when(sliced_ltc <= -0.5 ~ '< -0.5', 
+  #                                 sliced_ltc > -0.5 & sliced_ltc <= -0.001 ~ '-0.5 to 0',
+  #                                 sliced_ltc >= 0.001 & sliced_ltc < 0.5 ~ '0 to -0.5', 
+  #                                 sliced_ltc >= 0.5 ~ '> 0.5')) %>% 
+  #         mutate(temp = factor(temp, levels = c('< -0.5', 
+  #                                              '-0.5 to 0', 
+  #                                              '0 to -0.5', 
+  #                                              '> 0.5'))) %>% 
+  #         mutate(duration = case_when(Duration <= 10 ~ '5 years', Duration > 10 ~ '20 years')) %>% 
+  #         mutate(duration = factor(duration, levels = c('5 years', '20 years'))), 
+  #  aes(x = mean_invs + 0.1, y = mean_nuts + 0.001), shape = 21, colour = 'black') + 
+  facet_grid(duration ~ temp) + 
+  #xlab('\nInvasion potential (metric tonnes of cargo shipped)') + 
+  #ylab('Nutrient use (metric tonnes of nitrogen and phosphorous fertilizer used)\n') + 
+  xlab('\n   Invasion potential') + 
+  ylab('Nutrient use\n') + 
+  labs(colour = 'LRR') + 
+  guides(colour = guide_legend(override.aes = list(size = 3))) + 
+  ggtitle(expression("   Temperature ("*degree*"C)"))
 beep()
+
+dev.copy2pdf(file = '../figures/combined-drivers.pdf')
 
 
